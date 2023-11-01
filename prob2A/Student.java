fa7430766f7502ed72ebe6deae479386f8ee63af
Yahya Iddris Fatah <123132207+balatwist@users.noreply.github.com>
2023-11-01 02:02:47 -0500

    update

--- prob2A/Student.java
+++ prob2A/Student.java
@@ -2,8 +2,9 @@ package prob2A;
 
 public class Student {
     private String name;
-
+private GradeReport gra;
     public Student(String name) {
         this.name = name;
+        gra=new GradeReport;
     }
 }
