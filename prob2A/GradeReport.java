46b5fd2d9d6f2b70cf2a614326060622e1c718a1
Yahya Iddris Fatah <123132207+balatwist@users.noreply.github.com>
2023-11-01 01:57:42 -0500

    update

---
+++ prob2A/GradeReport.java
@@ -0,0 +1,9 @@
+package prob2A;
+
+public class GradeReport {
+    private String grade;
+
+    public GradeReport(String grade) {
+        this.grade = grade;
+    }
+}
---
+++ prob2A/Student.java
@@ -0,0 +1,9 @@
+package prob2A;
+
+public class Student {
+    private String name;
+
+    public Student(String name) {
+        this.name = name;
+    }
+}
